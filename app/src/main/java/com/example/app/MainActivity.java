43bdc88357dc189adb4cae522e102a0669ee7ccd
Shivam Sharma <kshivam654@gmail.com>
2019-12-27 22:43:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("lifecycle","onCreate invoked");
+        System.out.println("On Create");
+        System.out.println("On Create");
+        System.out.println("On Create");
+        System.out.println("On Create");
+        System.out.println("On Create");
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifecycle","onStart invoked");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifecycle","onResume invoked");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("On Pause");
+        System.out.println("On Pause");
+        System.out.println("On Pause");
+        System.out.println("On Pause");
+        System.out.println("On Pause");
+        Log.d("lifecycle","onPause invoked");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifecycle","onStop invoked");
+        System.out.println("On STOP");
+        System.out.println("On STOP");
+        System.out.println("On STOP");
+        System.out.println("On STOP");
+        System.out.println("On STOP");
+
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lifecycle","onRestart invoked");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifecycle","onDestroy invoked");
+        System.out.println("On Destroy");
+        System.out.println("On Destroy");
+        System.out.println("On Destroy");
+        System.out.println("On Destroy");
+        System.out.println("On Destroy");
+    }
+}
